00914547cc6b18d513bcc0331f49677f33449f0e
VicksNews11 <vicksnews11@gmail.com>
2025-10-16 15:12:38 +0530

    Delete Modules directory

--- Modules/resource_group/main.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "flipkart_rg" {
-  for_each = var.flipkart_rg
-  name     = each.value.name
-  location = each.value.location
-}
--- Modules/storage_account/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "stgflipkart" {
-  for_each                 = var.stgflipkart
-  name                     = each.value.name
-  resource_group_name      = each.value.resource_group_name
-  location                 = each.value.location
-  account_tier             = each.value.account_tier
-  account_replication_type = each.value.account_replication_type
-}
