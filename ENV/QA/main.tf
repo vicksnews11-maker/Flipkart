a6f580217eaab5f66ac0665027661a4b8196a1b8
Shahuls3 <vicksnews11@gmail.com>
2025-10-16 21:06:02 +0530

    added

---
+++ ENV/QA/main.tf
@@ -0,0 +1,11 @@
+module "resource_group" {
+  source      = "../../Modules/resource_group"
+  flipkart_rg = var.flipkart_rg
+}
+
+module "storage_account" {
+  source      = "../../Modules/storage_account"
+  stgflipkart = var.flipkart_rg
+  depends_on  = [module.resource_group]
+}
+#check
